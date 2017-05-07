4542d84d81499bd0d185a4efc3c6ab0dd06600b5
KevinMarquette <kevmar@gmail.com>
2017-05-06 21:12:05 -0700

    Added standard files

---
+++ FullModuleTemplate/root/docs/index.md
@@ -0,0 +1,4 @@
+# PSGraph Docs
+
+PSDeploy uses ReadTheDocs to host our documentation.  This allows us to keep our docs in the repository, without the various limitations that come with the built in GitHub repo wiki.
+
---
+++ FullModuleTemplate/root/tests/Feature.Tests.ps1
@@ -0,0 +1,9 @@
+$projectRoot = Resolve-Path "$PSScriptRoot\.."
+$moduleRoot = Split-Path (Resolve-Path "$projectRoot\*\*.psd1")
+$moduleName = Split-Path $moduleRoot -Leaf
+
+Import-Module (Join-Path $moduleRoot "$moduleName.psm1") -force
+
+Describe "Basic function feature tests" -Tags Build {
+
+}
