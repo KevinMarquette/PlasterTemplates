c63b98068d8d93c2b9c88df23958af09431576ce
Kevin Marquette <KMarquette@loandepot.com>
2018-01-27 12:25:24 -0800

    resolved #24 added quicker PSScriptAnalyser processing    tests all rules at once and dynamically created failed tests for each failed rule

--- FullModuleTemplate/tests/Project.Tests.ps1
+++ FullModuleTemplate/tests/Project.Tests.ps1
@@ -6,15 +6,25 @@ Describe "PSScriptAnalyzer rule-sets" -Tag Build {
     $Rules = Get-ScriptAnalyzerRule
     $scripts = Get-ChildItem $moduleRoot -Include *.ps1, *.psm1, *.psd1 -Recurse | where fullname -notmatch 'classes'
 
-    foreach ( $Script in $scripts ) 
+    foreach ( $Script in $scripts )
     {
         Context "Script '$($script.FullName)'" {
-
-            foreach ( $rule in $rules )
+            $results = Invoke-ScriptAnalyzer -Path $script.FullName -includeRule $Rules
+            if ($results)
             {
-                It "Rule [$rule]" {
+                foreach ($rule in $results)
+                {
+                    It $rule.RuleName {
+                        $message = "{0} Line {1}: {2}" -f $rule.Severity, $rule.Line, $rule.message
+                        $message | Should Be ""
+                    }
 
-                    (Invoke-ScriptAnalyzer -Path $script.FullName -IncludeRule $rule.RuleName ).Count | Should Be 0
+                }
+            }
+            else
+            {
+                It "Should not fail any rules" {
+                    $results | Should BeNullOrEmpty
                 }
             }
         }
